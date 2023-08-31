a8f1278cbbf6a44ec08b053152ce281ec4c2eabd
elyor04 <tuxtamuratovelyor@gmail.com>
2023-08-31 19:52:11 +0500

    just commit

--- AppMainWindow/appmainwindow.py
+++ AppMainWindow/appmainwindow.py
@@ -1,9 +1,9 @@
 """
 pyuic6 -o AppMainWindow/ui_form.py "path/to/file.ui"
 """
-from PyQt6.QtWidgets import QMainWindow, QWidget
+from PyQt6.QtWidgets import QMainWindow, QWidget, QLabel
 from PyQt6.QtCore import QTimer, Qt
-from PyQt6.QtGui import QImage, QPixmap
+from PyQt6.QtGui import QImage, QPixmap, QMouseEvent
 from .ui_form import Ui_MainWindow
 
 import face_recognition as fr
@@ -31,8 +31,11 @@ class AppMainWindow(QMainWindow, Ui_MainWindow):
 
         self.cam = cv.VideoCapture(0)
         self.timer = QTimer(self)
+        self.maxVideo = QLabel()
+
         self.known_face_encodings = []
         self.known_face_names = []
+        self.lastFrame = None
 
         self.setupUi(self)
         self._init()
@@ -42,7 +45,11 @@ class AppMainWindow(QMainWindow, Ui_MainWindow):
 
     def _init(self) -> None:
         self.videoLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.maxVideo.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.maxVideo.resize(600, 500)
         self.timer.timeout.connect(self.readCamera)
+        self.videoLabel.mouseDoubleClickEvent = self.videoLabel_doubleClicked
+        self.maxVideo.mouseDoubleClickEvent = lambda ev: self.maxVideo.hide()
         self.loadData("data")
         self.timer.start(2)
 
@@ -69,17 +76,16 @@ class AppMainWindow(QMainWindow, Ui_MainWindow):
                 if faceEncs:
                     self.known_face_encodings.append(faceEncs[0])
                     self.known_face_names.append(name)
+        print(str(self.known_face_encodings[0]))
 
     def readCamera(self) -> None:
-        if self.tabWidget.currentIndex() != 0:
-            return
-
         ret, frame = self.cam.read()
         if not ret:
             return
+        self.lastFrame = frame.copy()
 
         small_frame = cv.resize(
-            frame, (0, 0), fx=0.4, fy=0.4, interpolation=cv.INTER_AREA
+            frame, (0, 0), fx=0.3, fy=0.3, interpolation=cv.INTER_AREA
         )
         rgb_small_frame = cv.cvtColor(small_frame, cv.COLOR_BGR2RGB)
 
@@ -99,8 +105,8 @@ class AppMainWindow(QMainWindow, Ui_MainWindow):
             face_names.append(name)
 
         for (top, right, bottom, left), name in zip(face_locations, face_names):
-            top, right = int(top * 2.5), int(right * 2.5)
-            bottom, left = int(bottom * 2.5), int(left * 2.5)
+            top, right = int(top * 3.3), int(right * 3.3)
+            bottom, left = int(bottom * 3.3), int(left * 3.3)
 
             cv.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
             cv.rectangle(
@@ -112,8 +118,20 @@ class AppMainWindow(QMainWindow, Ui_MainWindow):
                 frame, name, (left + 8, bottom - 8), font, 1.5, (255, 255, 255), 2
             )
 
-        frame = self._resize(frame, (self.videoLabel.width(), self.videoLabel.height()))
-        self.videoLabel.setPixmap(cvMatToQPixmap(frame))
+        if self.maxVideo.isHidden():
+            frame = self._resize(
+                frame, (self.videoLabel.width(), self.videoLabel.height())
+            )
+            self.videoLabel.setPixmap(cvMatToQPixmap(frame))
+        else:
+            frame = self._resize(frame, (self.maxVideo.width(), self.maxVideo.height()))
+            self.maxVideo.setPixmap(cvMatToQPixmap(frame))
+
+    def videoLabel_doubleClicked(self, ev: QMouseEvent) -> None:
+        if self.maxVideo.isHidden():
+            self.maxVideo.showMaximized()
+        else:
+            self.maxVideo.hide()
 
 
 """from mysql.connector import connect
